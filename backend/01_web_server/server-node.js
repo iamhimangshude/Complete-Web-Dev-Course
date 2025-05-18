171421d167fa4b8b540a751aab459c399fbd41dd
iamhimangshude <dehimangshu03@gmail.com>
2025-05-18 10:07:27 +0530

    Completed the 01_web_server part and partial completion of 02_express part

---
+++ backend/01_web_server/server-node.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("hello world");
+  } else if (req.url === "/ice-tea") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Thanks for ordering ice tea, it's really hot!");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("404: Not found");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
