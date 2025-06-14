4b1ab03eadabdc246f43b33ef624cfa42d47f344
iamhimangshude <dehimangshu03@gmail.com>
2025-06-14 08:55:10 +0530

    feat: implement user registration functionality with error handling and route setup

--- vidtube/src/db/index.js
+++ vidtube/src/db/index.js
@@ -6,7 +6,9 @@ const connectDB = async () => {
     const connectionInstance = await mongoose.connect(
       `${process.env.MONGODB_URI}/${DB_NAME}`
     );
-    console.log(`\n MongoDB Connected! \n${connectionInstance}`);
+    console.log(
+      `\n MongoDB Connected! \nDB Host: ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("MongoDB Connection Error:", error);
     process.exit(1);
