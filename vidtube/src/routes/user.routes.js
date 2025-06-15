f98fdfe993c5a84d7645c338cf6774f5a34393d8
iamhimangshude <dehimangshu03@gmail.com>
2025-06-15 18:28:00 +0530

    Feat: Implemented User routes and controllers successfully

--- vidtube/src/routes/user.routes.js
+++ vidtube/src/routes/user.routes.js
@@ -1,10 +1,23 @@
 import { Router } from "express";
-import { registerUser, logoutUser } from "../controllers/user.controllers.js";
+import {
+  registerUser,
+  logoutUser,
+  loginUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getWatchHistory,
+} from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middlewares.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router();
 
+// INFO: insecured routes
 router.route("/register").post(
   upload.fields([
     {
@@ -19,12 +32,25 @@ router.route("/register").post(
   registerUser
 );
 
-// secured routes
-// user logout
+router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
+// INFO: secured routes
 router.route("/logout").post(
   verifyJWT, // first the middleware so created
   logoutUser // then the controller
 );
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/history").get(verifyJWT, getWatchHistory);
 
 export default router;
