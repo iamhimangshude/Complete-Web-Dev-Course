62ae21b0cded8d8ef0ac6bef66da6048419d8a8d
iamhimangshude <120447873+iamhimangshude@users.noreply.github.com>
2025-07-05 20:08:15 +0530

    Added react and vite folders

---
+++ react-course/01basicvite/src/App.jsx
@@ -0,0 +1,15 @@
+import Youtube from './youtube'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Vite + React app</h1>
+      <h1>Vite + React app</h1>
+      <Youtube/>
+    </>
+  )
+}
+
+export default App
