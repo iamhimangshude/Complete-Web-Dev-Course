1efc9852070c3d4d4639274b2b1c6d508ac576d6
iamhimangshude <120447873+iamhimangshude@users.noreply.github.com>
2025-07-05 20:38:34 +0530

    feat: Add custom rendering functionality and HTML structure for custom React app

--- react-course/01basicvite/src/App.jsx
+++ react-course/01basicvite/src/App.jsx
@@ -1,12 +1,12 @@
 import Youtube from './youtube'
 
 function App() {
+  const username = "hey"
   
-
   return (
     <>
-      <h1>Vite + React app</h1>
-      <h1>Vite + React app</h1>
+      <h1>Vite + React app {2+2}</h1>
+      <h1>Vite + React app {username === 'himangshu' ? 'Hii Himangshu': "Nah bye"}</h1>
       <Youtube/>
     </>
   )
