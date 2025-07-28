bc037b41d0190ea111e88f52352814d363fc7386
iamhimangshude <dehimangshu03@gmail.com>
2025-07-28 11:38:41 +0530

    added the verify email feature and sendmail helper to send verification emails.

---
+++ auth-app-nextjs/src/helpers/mailer.ts
@@ -0,0 +1,53 @@
+import nodemailer from "nodemailer";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const hashedToken = await bcryptjs.hash(userId.toString(), 10);
+
+    if (emailType === "VERIFY") {
+      await User.findByIdAndUpdate(userId, {
+        verifyToken: hashedToken,
+        verifyTokenExpiry: Date.now() + 3600000,
+      });
+    } else if (emailType === "RESET") {
+      await User.findByIdAndUpdate(userId, {
+        forgotPasswordToken: hashedToken,
+        forgotPasswordTokenExpiry: Date.now() + 3600000,
+      });
+    }
+    const transporter = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+
+      port: 2525,
+
+      auth: {
+        user: process.env.NODEMAILER_USER,
+
+        pass: process.env.NODEMAILER_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.FROM_EMAIL,
+      to: email,
+      subject:
+        emailType === "VERIFY" ? "Verfiy your email" : "Reset your password",
+      html: `<p>Click <a href="${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}">here</a> to ${
+        emailType === "VERIFY" ? "verify your email" : "reset your password"
+      }
+      or copy and paste the link below in your browser. <br>${
+        process.env.DOMAIN
+      }/verifyemail?token=${hashedToken}
+      </p>`,
+    };
+
+    const mailResponse = await transporter.sendMail(mailOptions);
+    return mailResponse;
+  } catch (error: any) {
+    throw new Error(error.message);
+  }
+};
