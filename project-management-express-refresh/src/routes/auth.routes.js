5d5019465a754161d57d30788484d7cfcbb75cba
iamhimangshude <iwillbreakproduction@gmail.com>
2025-11-05 10:40:24 +0530

    Added comments for easy understanding for new commers

--- project-management-express-refresh/src/routes/auth.routes.js
+++ project-management-express-refresh/src/routes/auth.routes.js
@@ -8,7 +8,15 @@ import {
 
 const router = Router();
 
-router.route("/register").post(userRegisterValidator(), validate, registerUser);
-router.route("/login").post(userLoginValidator(), validate, login);
+router.route("/register").post(
+  userRegisterValidator(), // Defines validation checks for register
+  validate, // Checks if all validations passes
+  registerUser, // hits the route after passing all the validations
+);
+router.route("/login").post(
+  userLoginValidator(), // Defines validation checks for login
+  validate, // Checks if all validations passes
+  login, // hits the route after passing all the validations
+);
 
 export default router;
